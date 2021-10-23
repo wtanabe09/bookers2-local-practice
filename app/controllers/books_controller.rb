064198169fbba74bd2c8e94625a386aa920c74c3
wtanabe09 <w.shoya.09@outlook.jp>
2021-10-23 15:55:33 +0900

    最初のコミット デバックまで完了

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    ensure_correct_user
+  end
+
+
+
+  def update
+    ensure_correct_user
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    ensure_correct_user
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
+  end
+
+end
