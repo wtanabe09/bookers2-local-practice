82b0e10026714f8f2110fde1c8a9f7473cb2ace6
wtanabe09 <w.shoya.09@outlook.jp>
2021-10-28 16:57:38 +0900

    （コンフリクト練習）マイグレーションした

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_084802) do
+ActiveRecord::Schema.define(version: 2021_10_28_074520) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
